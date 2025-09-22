9b474547a64f5e199ed4b5cdf120cc57fb0406d1
Krishna Kashyap <krishna.kashyap50@gmail.com>
2025-09-22 18:28:05 -0400

    commit 11

--- app/(Kambaz)/Account/Signin/page.tsx
+++ app/(Kambaz)/Account/Signin/page.tsx
@@ -3,6 +3,7 @@ export default function Signin() {
   return (
     <div id="wd-signin-screen">
       <h2>Assignment Done By : Krishna Kashyap Pasumarthi , Section : 04</h2>
+      <h4>*Github link present at the bottom part of the lab1 page</h4>
       <h3>Sign in</h3>
       <input className="wd-username" placeholder="username" defaultValue="Krishna Kashyap" /> <br />
       <input className="wd-password" placeholder="password" type="password" defaultValue="kashyap" /> <br />
