f0cbb2b165ab56af54848dc710c813f9facb6db5
Krishna Kashyap <krishna.kashyap50@gmail.com>
2025-10-06 16:11:08 -0400

    commit css

--- app/Labs/Lab2/BootstrapLists.tsx
+++ app/Labs/Lab2/BootstrapLists.tsx
@@ -1,26 +1,51 @@
 "use client";
-import { ListGroup } from "react-bootstrap";
+
+import { ListGroup, ListGroupItem } from "react-bootstrap";
 
 export default function BootstrapLists() {
   return (
-    <div id="wd-css-styling-lists" className="mb-4">
-      <h2>Favorite movies</h2>
-      <ListGroup>
-        <ListGroup.Item active>Aliens</ListGroup.Item>
-        <ListGroup.Item>Terminator</ListGroup.Item>
-        <ListGroup.Item>Blade Runner</ListGroup.Item>
-        <ListGroup.Item>Lord of the Ring</ListGroup.Item>
-        <ListGroup.Item disabled>Star Wars</ListGroup.Item>
-      </ListGroup>
+    <>
+      {/* 2.3.8 — Favorite movies */}
+      <div id="wd-css-styling-lists" className="mb-4">
+        <h2>Favorite movies</h2>
+        <ListGroup>
+          <ListGroupItem active>Aliens</ListGroupItem>
+          <ListGroupItem>Terminator</ListGroupItem>
+          <ListGroupItem>Blade Runner</ListGroupItem>
+          <ListGroupItem>Lord of the Ring</ListGroupItem>
+          <ListGroupItem disabled>Star Wars</ListGroupItem>
+        </ListGroup>
+      </div>
 
-      <h3 className="mt-3">Favorite books</h3>
-      <ListGroup>
-        <ListGroup.Item action href="https://en.wikipedia.org/wiki/Dune_(novel)">Dune</ListGroup.Item>
-        <ListGroup.Item action href="https://en.wikipedia.org/wiki/The_Lord_of_the_Rings">Lord of the Rings</ListGroup.Item>
-        <ListGroup.Item action href="https://en.wikipedia.org/wiki/The_Forever_War">The Forever War</ListGroup.Item>
-        <ListGroup.Item action disabled href="https://en.wikipedia.org/wiki/Ender%27s_Game">Ender's Game</ListGroup.Item>
-        <ListGroup.Item action onClick={() => alert("New book added")}>Add another book</ListGroup.Item>
-      </ListGroup>
-    </div>
+      {/* 2.3.9 — Favorite books hyperlink list */}
+      <div id="wd-css-hyperlink-list">
+        <h3>Favorite books</h3>
+        <ListGroup>
+          <ListGroupItem action active href="https://en.wikipedia.org/wiki/Dune_(novel)">
+            Dune
+          </ListGroupItem>
+          <ListGroupItem action href="https://en.wikipedia.org/wiki/The_Lord_of_the_Rings">
+            Lord of the Rings
+          </ListGroupItem>
+          <ListGroupItem action href="https://en.wikipedia.org/wiki/The_Forever_War">
+            The Forever War
+          </ListGroupItem>
+          <ListGroupItem action href="https://en.wikipedia.org/wiki/2001:_A_Space_Odyssey_(novel)">
+            2001 A Space Odyssey
+          </ListGroupItem>
+          <ListGroupItem
+            action
+            disabled
+            href="https://en.wikipedia.org/wiki/Ender&apos;s_Game"
+            title="Disabled link"
+          >
+            Enders Game
+          </ListGroupItem>
+          <ListGroupItem action onClick={() => alert("New book added")}>
+            Add another book
+          </ListGroupItem>
+        </ListGroup>
+      </div>
+    </>
   );
 }
