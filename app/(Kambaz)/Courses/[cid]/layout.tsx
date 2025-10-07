465f7c578f48c633c85e9e5b32573df0b667ef90
Krishna Kashyap <krishna.kashyap50@gmail.com>
2025-10-06 22:48:38 -0400

    a2 css bootstrap2

--- app/(Kambaz)/Courses/[cid]/layout.tsx
+++ app/(Kambaz)/Courses/[cid]/layout.tsx
@@ -1,5 +1,7 @@
 import type { ReactNode } from "react";
 import CourseNavigation from "./Navigation";
+import { FaBars } from "react-icons/fa";
+
 
 type Props = {
   children: ReactNode;
@@ -11,7 +13,12 @@ export default async function CoursesLayout({ children, params }: Props) {
 
   return (
     <div id="wd-courses">
-      <h2 className="text-danger">Course {cid}</h2>
+      <h2 className="text-danger d-flex align-items-center">
+        <span aria-hidden="true" className="me-3">
+          <FaBars className="fs-4 mb-1" />
+        </span>
+        <span>Course {cid}</span>
+      </h2>
       <hr />
       <div className="d-flex">
         {/* Sidebar: hidden on < md */}
